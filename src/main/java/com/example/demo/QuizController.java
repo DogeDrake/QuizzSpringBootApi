af49df65c8d19166c75c78617ec55c5f02bb21e5
Rodriguez, Luis I <luis.i.rodriguez@accenture.com>
2023-07-07 17:45:51 +0200

    Added CORS

--- src/main/java/com/example/demo/QuizController.java
+++ src/main/java/com/example/demo/QuizController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/quizzes")
+@CrossOrigin(origins = "http://localhost:4200")
 public class QuizController {
     private final QuestionRepository questionRepository;
     private final AnswerRepository answerRepository;
